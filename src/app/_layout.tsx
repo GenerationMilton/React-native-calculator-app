00e435666c27964d44408b6e44ed71c3c598fbe4
Milton Munoz <livemilton@gmail.com>
2026-08-08 16:43:36 -0500

    feature((HU-01)-HU(HU-01): :sparkles: project init

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,18 +1,16 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { useColorScheme } from 'react-native';
+import { View, Text } from "react-native";
+import React from "react";
+import { Slot } from "expo-router";
 
-import { AnimatedSplashOverlay } from '@/components/animated-icon';
-import AppTabs from '@/components/app-tabs';
-
-SplashScreen.preventAutoHideAsync();
-
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
+const RootLayout = () => {
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <AnimatedSplashOverlay />
-      <AppTabs />
-    </ThemeProvider>
+    <View>
+      <Text>Header</Text>
+
+      <Slot />
+      <Text>Footer</Text>
+    </View>
   );
-}
+};
+
+export default RootLayout;
