3ee1a24a129f06ffa674c99f890c7d4251a47c4a
Milton Munoz <livemilton@gmail.com>
2026-08-08 17:52:59 -0500

    feature((HU-01)-HU(HU-01): :sparkles: React Calculator: Global styles

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,12 +1,18 @@
 import { View, Text } from "react-native";
 import React from "react";
+import { globalStyles } from "../../styles/global-styles";
 
 const CalculatorApp = () => {
   return (
-    <View>
-      <Text style={{ fontSize: 50, fontFamily: "SpaceMono", color: "white" }}>
-        CalculatorApp
+    <View style={globalStyles.calculatorContainer}>
+      <Text
+        style={globalStyles.mainResult}
+        numberOfLines={1}
+        adjustsFontSizeToFit
+      >
+        50 x 500000000000000
       </Text>
+      <Text style={globalStyles.subResult}>250</Text>
     </View>
   );
 };
