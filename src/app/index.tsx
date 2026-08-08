38c1b7108ccd182f6b48eb6d6890fc6d42c1f3a8
Milton Munoz <livemilton@gmail.com>
2026-08-08 18:16:47 -0500

    feature((HU-01)-HU(HU-01): :sparkles: React Calculator: Custom Text + Default Props

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,18 +1,13 @@
 import { View, Text } from "react-native";
 import React from "react";
 import { globalStyles } from "../../styles/global-styles";
+import ThemeText from "../../components/ThemeText";
 
 const CalculatorApp = () => {
   return (
     <View style={globalStyles.calculatorContainer}>
-      <Text
-        style={globalStyles.mainResult}
-        numberOfLines={1}
-        adjustsFontSizeToFit
-      >
-        50 x 500000000000000
-      </Text>
-      <Text style={globalStyles.subResult}>250</Text>
+      <ThemeText variant="h1">50 x 500</ThemeText>
+      <ThemeText variant="h2">250</ThemeText>
     </View>
   );
 };
