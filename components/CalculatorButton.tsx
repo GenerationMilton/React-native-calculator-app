3beb15859aab02a525fd46f8d4822065d5809614
Milton Munoz <livemilton@gmail.com>
2026-08-08 19:03:04 -0500

    feature((HU-01)-HU(HU-01): :sparkles: React Calculator: Calculator buttons part 1

---
+++ components/CalculatorButton.tsx
@@ -0,0 +1,32 @@
+import { Text, Pressable } from "react-native";
+import { globalStyles } from "../styles/global-styles";
+import { Colors } from "@/constants/Colors";
+
+interface Props {
+  label: string;
+  color?: string;
+  blackText?: boolean;
+  onPress: () => void;
+}
+
+const CalculatorButton = ({ 
+    label, 
+    color= Colors.darkGray, 
+    blackText = false, 
+    onPress }: Props) => {
+  return (
+    <Pressable 
+        style={globalStyles.button} 
+        onPress={onPress}>
+      <Text
+        style={[
+          globalStyles.buttonText,
+          { color: blackText ? 'black' : 'white' },
+        ]}
+      >
+        {label}</Text>
+    </Pressable>
+  );
+};
+
+export default CalculatorButton;
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,12 +2,42 @@ import { View, Text } from "react-native";
 import React from "react";
 import { globalStyles } from "../../styles/global-styles";
 import ThemeText from "../../components/ThemeText";
+import CalculatorButton from "../../components/CalculatorButton";
+import { Colors } from "@/constants/Colors";
 
 const CalculatorApp = () => {
   return (
     <View style={globalStyles.calculatorContainer}>
-      <ThemeText variant="h1">50 x 500</ThemeText>
-      <ThemeText variant="h2">250</ThemeText>
+      {/*Resultados */}
+      <View style={{ paddingHorizontal: 30, marginBottom: 20 }}>
+        <ThemeText variant="h1">50 x 500</ThemeText>
+        <ThemeText variant="h2">250</ThemeText>
+      </View>
+
+      {/*Filas de botones */}
+
+      <View style={globalStyles.row}>
+        {/* Boton1 */}
+        <CalculatorButton
+          label="C"
+          blackText
+          color={Colors.lightGray}
+          onPress={() => console.log("C")}
+        />
+        <CalculatorButton
+          label="+/-"
+          blackText
+          color={Colors.lightGray}
+          onPress={() => console.log("+/-")}
+        />
+        <CalculatorButton
+          label="del"
+          blackText
+          color={Colors.lightGray}
+          onPress={() => console.log("del")}
+        />
+        <CalculatorButton label="÷" onPress={() => console.log("÷")} />
+      </View>
     </View>
   );
 };
